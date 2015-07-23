d3ae460c9a60e90a0500204764b45f7001431dcc
Michael Waterworth <mike@3equals.co.uk>
2015-07-23 10:13:33 +0100

    Use a safe image downloader that won't leak

--- app/src/main/java/uk/co/threeequals/ratemyview/TheirViewActivity.java
+++ app/src/main/java/uk/co/threeequals/ratemyview/TheirViewActivity.java
@@ -1,23 +1,17 @@
 package uk.co.threeequals.ratemyview;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 
 public class TheirViewActivity extends AppCompatActivity {
@@ -57,17 +51,17 @@ public class TheirViewActivity extends AppCompatActivity {
 		}
 		
 		img = (ImageView) findViewById(R.id.theirImage);
-		img.setOnClickListener(
-			new OnClickListener() {           
-				@Override
-				public void onClick(View v) {
-						Intent i = new Intent(getBaseContext(), TheirImageActivity.class);
-						if(theirImageUri != null){
-							i.putExtra("Image", theirImageUri);
-						}
-						startActivity(i);
-			}    
-		});
+//		img.setOnClickListener(
+//			new OnClickListener() {
+//				@Override
+//				public void onClick(View v) {
+//						Intent i = new Intent(getBaseContext(), TheirImageActivity.class);
+//						if(theirImageUri != null){
+//							i.putExtra("Image", theirImageUri);
+//						}
+//						startActivity(i);
+//			}
+//		});
 				
 		if(item.getPhotoLocation() != null){
 			theirImageUri = item.getPhotoLocation();
@@ -85,7 +79,6 @@ public class TheirViewActivity extends AppCompatActivity {
 		    try {
 		    	File fileExists = new File(tempDir.getAbsolutePath()+ item.getPhoto());
 		    	if(fileExists.exists()){
-			    	//System.out.println("Loading image from local cache");
 			    	loadImageFromFile(fileExists);
 			    } else {
 			    	loadImageFromUrl(item.getPhoto());
@@ -98,14 +91,9 @@ public class TheirViewActivity extends AppCompatActivity {
 	}
 	
 	private void loadImageFromUrl(String photoUrl){
-		Handler mHandler = new Handler() {//TODO - may be an issue
-            @Override public void handleMessage(Message msg) { 
-            	String i=(String)msg.obj;
-				File imgFile = new File(i);
-				loadImageFromFile(imgFile);
-            }
-        };
-		new DownloadImageTask(mHandler).execute(photoUrl);
+        photoUrl = getString(R.string.base_url) + getString(R.string.assets_path) + photoUrl;
+        new DownloadImageTask((ImageView) findViewById(R.id.theirImage))
+                .execute(photoUrl);
 	}
 	
 	private void loadImageFromFile(File f){
@@ -115,9 +103,6 @@ public class TheirViewActivity extends AppCompatActivity {
 	}
 	
     protected void onStop(){
-//    	if(tileProviderSatellite != null){
-//    		tileProviderSatellite.detach();
-//    	}
     	super.onStop();
     }
     
@@ -130,52 +115,31 @@ public class TheirViewActivity extends AppCompatActivity {
 	public void onResume() {
 		super.onResume();
 	}
-	
-	private class DownloadImageTask extends AsyncTask<String, Void, String> {
-		Handler mHandler;
-		
-		  public DownloadImageTask(Handler mHandler) {
-		      this.mHandler = mHandler;
-		      //bmImage.setImageResource(R.drawable.loading);
-		  }
-		
-		  protected String doInBackground(String... urls) {
-		      String urldisplay = urls[0];
-		      //String saveUrl = "";//Use ID of Remote View
-		      try {
-		        InputStream in = new java.net.URL(getString(R.string.base_url) + "assets/uploads/" + urldisplay).openStream();
-		        File tempDir = Environment.getExternalStorageDirectory();
-			    tempDir = new File(tempDir.getAbsolutePath()+"/.temp/");
-			    if(!tempDir.exists()) {
-			        tempDir.mkdir();
-			    }
-		        FileOutputStream outputStream = new FileOutputStream(new File(tempDir.getAbsolutePath() + urldisplay));
-		        
-		        
-		        int read;
-				byte[] bytes = new byte[1024];
-		 
-				while ((read = in.read(bytes)) != -1) {
-					outputStream.write(bytes, 0, read);
-				}
-				outputStream.close();
-				return tempDir.getAbsolutePath() + urldisplay;
-		      } catch (Exception e) {
-		          Log.e("Error", e.getMessage());
-		          e.printStackTrace();
-		      }
-		      return null;
-		  }
-		
-		  protected void onPostExecute(String result) {
-				//Somehow return the String of the Local Uri - result
-				super.onPostExecute(result);
-				Message msg=new Message();
-				msg.obj=result;
-				mHandler.sendMessage(msg);
-				//bmImage.setImageBitmap(result);
-		  }
-	}
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 	
 //	private String getDistance(){
 //		//return distance(lat, lng, lat, lng"k") + " km";
